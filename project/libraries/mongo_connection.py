ba610b8b706dd722a4b89a3ef9f83f6219e41e5e
Alessandro Kuz <alessandrokuz@gmail.com>
2023-06-13 14:17:13 +0200

    Update mongo_connection.py to act only as a lib
    removed the if __name__ == "__main__"

--- project/libraries/mongo_connection.py
+++ project/libraries/mongo_connection.py
@@ -1,6 +1,5 @@
 import pymongo.errors
 from pymongo import MongoClient
-import pandas as pd
 
 
 def client_connection():
@@ -28,17 +27,3 @@ def collection_connection(db_name: str, collection_name: str):
         return db_selected[collection_name]
     else:
         raise pymongo.errors.OperationFailure(f"Collection '{collection_name}' not found!")
-
-
-if __name__ == "__main__":
-    try:
-        collection = collection_connection(db_name='Esempio_db', collection_name='Macchine')
-        if collection:
-            item_details = collection.find()
-            df_items = pd.DataFrame(item_details)
-
-            print(df_items.head())
-            print(df_items.describe())
-            print(df_items.info())
-    except Exception as err:
-        print(f"An error occurred: {err}")
